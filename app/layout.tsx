b04eb6f8cf53bda1708f0176155a7b13972f67bb
Hoang Tuan Anh <anhht.ca@gmail.com>
2025-01-21 09:44:12 +0700

    mod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import "../global.css";
 import { Inter } from "@next/font/google";
 import LocalFont from "@next/font/local";
 import { Metadata } from "next";
+import "../global.css";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "anhht.me",
+    template: "%s | anhht.me",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "anhht.me",
+    description: "Co-founder of unkey.dev and founder of planetfall.io",
+    url: "https://anhht.me",
+    siteName: "anhht.me",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://anhht.me/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "anhht",
     card: "summary_large_image",
   },
   icons: {
@@ -66,8 +65,9 @@ export default function RootLayout({
         <Analytics />
       </head>
       <body
-        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-          }`}
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
@@ -29,7 +28,7 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        anhht
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -42,10 +41,10 @@ export default function Home() {
             className="underline duration-500 hover:text-zinc-300"
           >
             unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          </Link>{" "}
+          to solve API authentication and authorization for developers.
         </h2>
       </div>
     </div>
   );
-
 }
