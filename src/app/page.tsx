52dd423484ffa0fb78b87e1f459a960745938f7d
Hoang Tuan Anh <anhht.ca@gmail.com>
2025-01-23 09:20:05 +0700

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,16 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ul className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Welcome to my portfolio site{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
+              anhht.me
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+          {/* <li>Save and see your changes instantly.</li> */}
+        </ul>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -37,15 +37,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Projects
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://vietcoding.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read my Blog
           </a>
         </div>
       </main>
