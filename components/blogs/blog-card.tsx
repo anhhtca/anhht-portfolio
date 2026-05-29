d472c5992a7788943f3d8b5600ffad26d0589049
Hoang Tuan Anh <anhht.ca@gmail.com>
2026-05-29 10:07:28 +0700

    Add responsive image sizes to BlogCard and ProjectCard components

--- components/blogs/blog-card.tsx
+++ components/blogs/blog-card.tsx
@@ -30,6 +30,7 @@ export default function BlogCard({ blog }: BlogCardProps) {
               src={blog.coverImage}
               alt={blog.title}
               fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               className="object-cover transition-transform duration-500 group-hover:scale-105"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-background/60 to-transparent" />
